152dbd88994aa8b0b61d233e84eb65d3061c4309
ionioxa <ion7ioxa@gmail.com>
2017-07-19 22:51:12 -0400

    Initial Commit

---
+++ test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  test1
+//
+//  Created by Ion on 2017-07-19.
+//  Copyright © 2017 Ion. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
